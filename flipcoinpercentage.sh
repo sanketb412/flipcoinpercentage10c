839cc33f4d3c5f3574f2f62a3b9ddf419fd7a02e
sanketb412 <sanketbagde38@gmail.com>
2021-04-22 11:45:43 +0530

    Added code for singlet and winner percentage

--- flipcoinpercentage.sh
+++ flipcoinpercentage.sh
@@ -2,13 +2,35 @@ echo
 echo "# flipcoinpercentage"
 echo
 
-declare -A singlet
-singlet[ ((count++)) ]=$((RANDOM%2))
+echo "#UC1"
+echo
+count=0
 
-if [ ${singlet[@]} -eq 1 ]
+for (( i=1; i<=10; i++ ))
+do
+	declare -A singlet
+	singlet[ ((count++)) ]=$((RANDOM%2))
+
+	if [ ${singlet[@]} -eq 1 ]
+	then
+		echo "Head"
+		((Head++))
+	else
+		echo "Tail"
+		((Tail++))
+	fi
+
+	((count++))
+done
+echo
+echo "number of heads win = $Head"
+echo "number of tails win = $Tail"
+echo
+if [ $Head -gt $Tail ];
 then
-	echo "Head"
+        x=$((($Head-$Tail)*100/$Head))
+        echo "Percentage of the singlet between Head to Tail is $x%"
 else
-	echo "Tail"
+        y=$((($Tail-$Head)*100/$Tail))
+        echo "Percentage of the singlet between Tail to Head is $y% "
 fi
-
